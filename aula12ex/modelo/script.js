a4fea5ee0f9586e85733b14c941f920d709a1586
EduardoRocha01 <120874588+EduardoRocha01@users.noreply.github.com>
2022-12-22 14:33:39 -0400

    novo

--- aula12ex/modelo/script.js
+++ aula12ex/modelo/script.js
@@ -1,1 +1,23 @@
-alert('ola')
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 5
+
+    msg.innerHTML = `agora são ${hora} horas.`
+
+    if(hora>=0 && hora < 12){
+        //bom dia
+       img.innerHTML.src = './imagem/manha.png'
+    }else if(hora >=12 && hora <= 18){
+        //BOA TARDE
+    
+    }else {
+        //BOA NOITE
+    
+
+    }
+}
+
